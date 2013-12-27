b318824efa9cef9102b6519f521450fd9a107473
Tommi Reiman <tommi.reiman@metosin.fi>
2013-12-27 16:08:07 +0200

    update deps, + schema

--- project.clj
+++ project.clj
@@ -7,11 +7,12 @@
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [metosin/compojure-api "0.0.1-SNAPSHOT"]
-                 [cheshire "5.2.0"]
-                 [compojure "1.1.5"]]
+                 [cheshire "5.3.0"]
+                 [prismatic/schema "0.1.10"]
+                 [compojure "1.1.6"]]
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler compojure.api.example.handler/app}
   :profiles {:dev {:plugins [[lein-clojars "0.9.1"]]
                    :dependencies [[ring-mock "0.1.5"]
-                                  [midje "1.5.1"]
-                                  [clj-http "0.7.7" :exclusions [commons-codec]]]}})
+                                  [midje "1.6.0"]
+                                  [clj-http "0.7.8" :exclusions [commons-codec]]]}})
