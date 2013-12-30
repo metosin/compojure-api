a0322c93f2c5c3eabcd70e072d59ba0fa181673d
Tommi Reiman <tommi.reiman@metosin.fi>
2013-12-30 23:47:08 +0200

    dry

--- src/compojure/api/example/handler.clj
+++ src/compojure/api/example/handler.clj
@@ -19,19 +19,13 @@
 
 (defapi api
   (swagger-docs "/api/docs"
-    ;;:apiVersion "1.0.0"
-    ;;:termsOfServiceUrl "http://www.metosin.fi"
-    ;;:contact "tommi@metosin.fi"
-    ;;:licence "Apache 2.0"
-    ;;:licenseUrl "http://www.apache.org/licenses/LICENSE-2.0.html"
     :title "Cool api"
     :description "Compojure Sample Web Api")
   (swaggered :sample
     :description "sample api"
     (context "/api" []
       (context "/pizza" []
-        (^{:model Pizza} GET "/:id" [id]
-          (response quatro))
+        (^{:model Pizza} GET "/:id" [id] (response quatro))
         (POST "" [] (response {:a 1}))))))
 
 (def app
