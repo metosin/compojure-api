e6a4bef9ba8216ff3815be97bdc2bbd11887b391
Tommi Reiman <tommi.reiman@metosin.fi>
2013-12-30 12:33:16 +0200

    support for optional keys, purge empty required

--- src/compojure/api/schema.clj
+++ src/compojure/api/schema.clj
@@ -6,22 +6,25 @@
   (filter s/required-key? (keys schema)))
 
 (defmulti json-type identity)
-(defmethod json-type s/Int [_] {:type "integer"
-                                :format "int64"})
+(defmethod json-type s/Int    [_] {:type "integer" :format "int64"})
 (defmethod json-type s/String [_] {:type "string"})
 (defmethod json-type sString  [_] {:type "string"})
 
 (defn properties [schema]
   (into {}
-    (for [[k v] schema]
+    (for [[k v] schema
+          :let [k (s/explicit-schema-key k)]]
       [k (merge (meta v) (json-type v))])))
 
 (defn transform [schema-symbol]
   {:pre [(symbol? schema-symbol)]}
-  (let [schema (eval schema-symbol)]
-    {:id (str schema-symbol)
-     :required (required-keys schema)
-     :properties (properties schema)}))
+  (let [schema (eval schema-symbol)
+        required (required-keys schema)
+        target {:id (str schema-symbol)
+                :properties (properties schema)}]
+    (if (seq required)
+      (assoc target :required required)
+      target)))
 
 (def sString
   "Clojure String"
--- test/compojure/api/schema_test.clj
+++ test/compojure/api/schema_test.clj
@@ -4,16 +4,35 @@
             [schema.macros :as sm]
             [compojure.api.schema :refer :all]))
 
-(def Tag {:id   (with-meta s/Int {:description "Unique identifier for the tag"})
-          :name (with-meta sString {:description "Friendly name for the tag"})})
-
+(def Tag {(s/optional-key :id)   (with-meta s/Int {:description "Unique identifier for the tag"})
+          (s/optional-key :name) (with-meta sString {:description "Friendly name for the tag"})})
 (def Tag' {:id "Tag"
-           :required [:id :name]
            :properties {:id {:type "integer"
                              :format "int64"
                              :description "Unique identifier for the tag"}
                         :name {:type "string"
                                :description "Friendly name for the tag"}}})
 
+(def Category  {:id (with-meta s/Int {:description "Category unique identifier"
+                                      :minimum "0.0"
+                                      :maximum "100.0"})
+                :name (with-meta sString {:description "Name of the category"
+                                          :minimum "0.0"
+                                          :maximum "100.0"})})
+
+(def Category' {:id "Category"
+                :properties {:id {:type "integer"
+                                  :format "int64"
+                                  :description "Category unique identifier"
+                                  :minimum "0.0"
+                                  :maximum "100.0"}
+                             :name {:type "string"
+                                    :description "Name of the category"
+                                    :minimum "0.0"
+                                    :maximum "100.0"}}})
+
 (fact "simple schema"
   (transform 'Tag) => Tag')
+
+#_(fact "Swagger Petstore example"
+  (transform 'Category) => Category')
