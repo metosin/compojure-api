ce4f72e670eec4e11ec03f667e080b8d1fccf94d
Tommi Reiman <tommi.reiman@metosin.fi>
2014-01-18 10:27:42 +0200

    cleaner dsl & pimp-namespace for redefining compile-route to accept meta-data. Adding meta-data to compojure routes now is side-effect free

--- src/compojure/api/dsl.clj
+++ src/compojure/api/dsl.clj
@@ -1,44 +1,28 @@
 (ns compojure.api.dsl
   (:require [compojure.core :refer :all]
+            [compojure.api.pimp]
             [compojure.api.schema :as schema]
             [ring.util.response :as response]
             [compojure.api.common :refer :all]))
+
+;;
+;; common
+;;
 
 (defn ok
   "status 200"
   [body] (response/response body))
 
-(defmacro GET* [path arg & body]
-  (let [[parameters body] (extract-parameters body)]
-    `(with-meta (GET ~path ~arg ~@body) ~parameters)))
+;;
+;; routes
+;;
 
-;; FIXME: evaluates with-meta for all requests
-#_(defmacro with-body [method path arg & body]
-  (println body)
-  (let [[parameters body] (extract-fn-parameters body)]
-    (if-let [[body-name body-model body-meta] (:body parameters)]
-      (let [parameters (-> parameters
-                         (dissoc :body)
-                         schema/purge-model-vars
-                         (update-in [:parameters] conj
-                           (merge
-                             {:name (-> body-model schema/purge-model-var name-of .toLowerCase)
-                              :description ""
-                              :required "true"}
-                             body-meta
-                             {:paramType "body"
-                              :type body-model}))
-                         (update-in [:parameters] vec))]
-        `(fn [req#]
-           (let [{~body-name :params} req#]
-             ((with-meta ('~method ~path ~arg ~@body) ~parameters) req#))))
-      `(with-meta ('~method ~path ~arg ~@body) ~parameters))))
-
-#_(defmacro POST* [path arg & body]
-  `(with-body #'POST ~path ~arg ~@body))
-
-#_(defmacro PUT* [path arg & body]
-  `(with-body #'PUT ~path ~arg ~@body))
+(defmacro GET* [path arg & body] `(GET ~path ~arg ~@body))
+(defmacro DELETE* [path arg & body] `(DELETE ~path ~arg ~@body))
+(defmacro HEAD* [path arg & body] `(HEAD ~path ~arg ~@body))
+(defmacro OPTIONS* [path arg & body] `(OPTIONS ~path ~arg ~@body))
+(defmacro PATCH* [path arg & body] `(PATCH ~path ~arg ~@body))
+(defmacro ANY* [path arg & body] `(ANY ~path ~arg ~@body))
 
 (defmacro POST* [path arg & body]
   (let [[parameters body] (extract-parameters body)]
@@ -57,8 +41,8 @@
                          (update-in [:parameters] vec))]
         `(fn [req#]
            (let [{~body-name :params} req#]
-             ((with-meta (POST ~path ~arg ~@body) ~parameters) req#))))
-      `(with-meta (POST ~path ~arg ~@body) ~parameters))))
+             ((POST ~path ~arg ~parameters ~@body) req#))))
+      `(POST ~path ~arg ~parameters ~@body))))
 
 (defmacro PUT* [path arg & body]
   (let [[parameters body] (extract-parameters body)]
@@ -77,9 +61,5 @@
                          (update-in [:parameters] vec))]
         `(fn [req#]
            (let [{~body-name :params} req#]
-             ((with-meta (PUT ~path ~arg ~@body) ~parameters) req#))))
-      `(with-meta (PUT ~path ~arg ~@body) ~parameters))))
-
-(defmacro DELETE* [path arg & body]
-  (let [[parameters body] (extract-fn-parameters body)]
-    `(with-meta (DELETE ~path ~arg ~@body) ~parameters)))
+             ((PUT ~path ~arg ~parameters ~@body) req#))))
+      `(PUT ~path ~arg ~parameters ~@body))))
---
+++ src/compojure/api/pimp.clj
@@ -0,0 +1,21 @@
+(ns compojure.api.pimp
+  "var-pimp of of compojure.core/compile-route to support meta-data"
+  (:require [compojure.core]
+            [clout.core :as clout]
+            [compojure.api.common :as common]))
+
+(ns compojure.core)
+(defonce compile-route-orinal compile-route)
+(defn compile-route-pimped
+  "Compile a route in the form (method path & {optional meta-data} body) into a function."
+  [method route bindings body]
+  (let [[meta-data body] (compojure.api.common/extract-parameters body)]
+    (println "compiling:" method route)
+    (with-meta
+      `(make-route
+         ~method ~(prepare-route route)
+         (fn [request#]
+           (let-request [~bindings request#] ~@body)))
+      meta-data)))
+(def compile-route compile-route-pimped)
+(ns compojure.api.pimp)
