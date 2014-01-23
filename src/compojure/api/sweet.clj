f98134fad4a0e02eae011697215d0d33f63748b0
Tommi Reiman <tommi.reiman@metosin.fi>
2014-01-23 15:58:50 +0200

    sweet

--- src/compojure/api/sweet.clj
+++ src/compojure/api/sweet.clj
@@ -1,12 +1,41 @@
 (ns compojure.api.sweet
-  (:require [potemkin :refer :all]))
+  (:require [potemkin :refer :all]
+            compojure.core
+            compojure.api.core
+            compojure.swagger.core))
 
 (import-vars
+
+  ;; compojure routing
+  [compojure.core
+   let-request
+   routing
+   routes
+   defroutes
+   context
+   let-routes]
+
+  ;; with enchanced methods
+  [compojure.api.core
+   GET*
+   ANY*
+   HEAD*
+   PATCH*
+   DELETE*
+   OPTIONS*
+   POST*
+   PUT*]
+
+  ;; swaggered
+  [compojure.swagger.core
+   swagger-ui
+   swagger-docs
+   swaggered]
+
+  ;; common stuff
   [compojure.api.core
     ok
     ->Long
     defapi
-    with-middeware]
-  [clojure.data
-    diff])
+    with-middleware])
 
