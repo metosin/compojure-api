35423d8ba98efceb970eec4877f41847745ae510
Tommi Reiman <tommi@iki.fi>
2014-05-05 08:22:07 +0300

    separare defapis in tests for clarity

--- test/compojure/api/core_integration_test.clj
+++ test/compojure/api/core_integration_test.clj
@@ -45,7 +45,7 @@
 
 (def invalid-user {:id 1 :name "Jorma" :age 50})
 
-(def app-name (str (gensym)))
+(def +name+ (str (gensym)))
 
 ;;
 ;; Middleware setup
@@ -68,22 +68,44 @@
 ;; Facts
 ;;
 
-(facts "for a compojure-api app"
+(facts "e2e"
   (background
-    (after :contents (swap! swagger/swagger dissoc app-name)))
+    (after :contents (swap! swagger/swagger dissoc +name+)))
 
-  (defapi api
-    (middlewares [(middleware* 1) (middleware* 2)]
-      (swaggered app-name
 
-        (context "/middlewares" []
-          (GET* "/simple" req (reply-mw* req))
-          (middlewares [(middleware* 3) (middleware* 4)]
-            (GET* "/nested" req (reply-mw* req))
-            (GET* "/nested-declared" req
-              :middlewares [(middleware* 5) (middleware* 6)]
-              (reply-mw* req))))
 
+  (facts "middlewares"
+    (defapi api
+      (middlewares [(middleware* 1) (middleware* 2)]
+        (swaggered +name+
+          (context "/middlewares" []
+            (GET* "/simple" req (reply-mw* req))
+            (middlewares [(middleware* 3) (middleware* 4)]
+              (GET* "/nested" req (reply-mw* req))
+              (GET* "/nested-declared" req
+                :middlewares [(middleware* 5) (middleware* 6)]
+                (reply-mw* req)))))))
+
+    (fact "are applied left-to-right"
+      (let [[status body headers] (get* api "/middlewares/simple" {})]
+        status => 200
+        (get headers mw*) => "12721"))
+
+    (fact "are applied left-to-right closest one first"
+      (let [[status body headers] (get* api "/middlewares/nested" {})]
+        status => 200
+        (get headers mw*) => "123474321"))
+
+    (fact "are applied left-to-right for both nested & declared cloest one first"
+      (let [[status body headers] (get* api "/middlewares/nested-declared" {})]
+        status => 200
+        (get headers mw*) => "1234567654321")))
+
+
+
+  (fact ":body, :query and :return"
+    (defapi api
+      (swaggered +name+
         (context "/models" []
           (GET* "/pertti" []
             :return User
@@ -111,86 +133,7 @@
             (ok users))
           (POST* "/user_legacy" {user :body-params}
             :return User
-            (ok user)))
-
-        (context "/smart" []
-          (GET* "/plus" []
-            :query-params [x :- Long y :- Long]
-            (ok {:total (+ x y)}))
-          (GET* "/multiply/:x/:y" []
-            :path-params [x :- Long y :- Long]
-            (ok {:total (* x y)}))
-          (POST* "/minus" []
-            :body-params [x :- Long {y :- Long 1}]
-            (ok {:total (- x y)})))
-
-        (context "/destructuring" []
-          (GET* "/regular" {{:keys [a]} :params}
-            (ok {:a a
-                 :b (-> +compojure-api-request+ :params :b)}))
-          (GET* "/regular2" {:as req}
-            (ok {:a (-> req :params :a)
-                 :b (-> +compojure-api-request+ :params :b)}))
-          (GET* "/vector" [a]
-            (ok {:a a
-                 :b (-> +compojure-api-request+ :params :b)}))
-          (GET* "/vector2" [:as req]
-            (ok {:a (-> req :params :a)
-                 :b (-> +compojure-api-request+ :params :b)}))
-          (GET* "/symbol" req
-            (ok {:a (-> req :params :a)
-                 :b (-> +compojure-api-request+ :params :b)}))
-          (GET* "/integrated" [a] :query-params [b]
-            (ok {:a a
-                 :b b}))))))
-
-  (facts "middlewares"
-
-    (fact "applies middlewares left-to-right"
-      (let [[status body headers] (get* api "/middlewares/simple" {})]
-        status => 200
-        (get headers mw*) => "12721"))
-
-    (fact "applies nested middlewares left-to-right closest one first"
-      (let [[status body headers] (get* api "/middlewares/nested" {})]
-        status => 200
-        (get headers mw*) => "123474321"))
-
-    (fact "applies nested & declared middlewares left-to-right closest one first"
-      (let [[status body headers] (get* api "/middlewares/nested-declared" {})]
-        status => 200
-        (get headers mw*) => "1234567654321")))
-
-  (facts "compojure destucturing"
-    (doseq [uri ["regular" "regular2" "vector" "vector2" "symbol" "integrated"]]
-      (fact {:midje/description uri}
-        (let [[status body] (get* api (str "/destructuring/" uri) {:a "a" :b "b"})]
-          status => 200
-          body => {:a "a" :b "b"}))))
-
-  (facts "query, path and body parameter smart destucturing"
-
-    (fact "query-parameters"
-      (let [[status body] (get* api "/smart/plus" {:x 2 :y 3})]
-        status => 200
-        body => {:total 5}))
-
-    (fact "query-parameters"
-      (let [[status body] (get* api "/smart/multiply/2/3")]
-        status => 200
-        body => {:total 6}))
-
-    (fact "body-parameters"
-      (let [[status body] (post* api "/smart/minus" (json {:x 2 :y 3}))]
-        status => 200
-        body => {:total -1}))
-
-    (fact "default parameters"
-      (let [[status body] (post* api "/smart/minus" (json {:x 2}))]
-        status => 200
-        body => {:total 1})))
-
-  (facts "models"
+            (ok user)))))
 
     (fact "GET*"
       (let [[status body] (get* api "/models/pertti")]
@@ -237,11 +180,80 @@
         (:type body) => "json-parse-exception"
         (:message body) => truthy)))
 
+
+
+  (fact ":query-params, :path-params & :body-params"
+    (defapi api
+      (swaggered +name+
+        (context "/smart" []
+          (GET* "/plus" []
+            :query-params [x :- Long y :- Long]
+            (ok {:total (+ x y)}))
+          (GET* "/multiply/:x/:y" []
+            :path-params [x :- Long y :- Long]
+            (ok {:total (* x y)}))
+          (POST* "/minus" []
+            :body-params [x :- Long {y :- Long 1}]
+            (ok {:total (- x y)})))))
+
+    (fact "query-parameters"
+      (let [[status body] (get* api "/smart/plus" {:x 2 :y 3})]
+        status => 200
+        body => {:total 5}))
+
+    (fact "query-parameters"
+      (let [[status body] (get* api "/smart/multiply/2/3")]
+        status => 200
+        body => {:total 6}))
+
+    (fact "body-parameters"
+      (let [[status body] (post* api "/smart/minus" (json {:x 2 :y 3}))]
+        status => 200
+        body => {:total -1}))
+
+    (fact "default parameters"
+      (let [[status body] (post* api "/smart/minus" (json {:x 2}))]
+        status => 200
+        body => {:total 1})))
+
+
+
+  (fact "compojure destructuring support"
+    (defapi api
+      (swaggered +name+
+        (context "/destructuring" []
+          (GET* "/regular" {{:keys [a]} :params}
+            (ok {:a a
+                 :b (-> +compojure-api-request+ :params :b)}))
+          (GET* "/regular2" {:as req}
+            (ok {:a (-> req :params :a)
+                 :b (-> +compojure-api-request+ :params :b)}))
+          (GET* "/vector" [a]
+            (ok {:a a
+                 :b (-> +compojure-api-request+ :params :b)}))
+          (GET* "/vector2" [:as req]
+            (ok {:a (-> req :params :a)
+                 :b (-> +compojure-api-request+ :params :b)}))
+          (GET* "/symbol" req
+            (ok {:a (-> req :params :a)
+                 :b (-> +compojure-api-request+ :params :b)}))
+          (GET* "/integrated" [a] :query-params [b]
+            (ok {:a a
+                 :b b})))))
+
+    (doseq [uri ["regular" "regular2" "vector" "vector2" "symbol" "integrated"]]
+      (fact {:midje/description uri}
+        (let [[status body] (get* api (str "/destructuring/" uri) {:a "a" :b "b"})]
+          status => 200
+          body => {:a "a" :b "b"}))))
+
+
+
   (fact "counting execution times, issue #19"
     (let [execution-times (atom 0)]
 
       (defapi api
-        (swaggered app-name
+        (swaggered +name+
           (GET* "/user" []
             :return User
             :query  [user User]
