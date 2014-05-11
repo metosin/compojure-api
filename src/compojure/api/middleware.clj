24ed6806b1319c89031c4168aec888db01c47fdd
Juho Teperi <juho.teperi@iki.fi>
2014-05-11 20:22:53 +0300

    Remove dependency on compojure.handler
    We now define all middlewares for "apis" ourselves. Before
    `compojure.handler/api` has used as base but that namespace uses
    multipart middleware which depends on servlet-api which we don't want
    to depend on.

--- src/compojure/api/middleware.clj
+++ src/compojure/api/middleware.clj
@@ -1,12 +1,14 @@
 (ns compojure.api.middleware
   (:require [clojure.walk16 :as walk]
-            [compojure.handler :as compojure]
             [ring.util.response :refer [response content-type redirect]]
             [cheshire.core :as cheshire]
             [clojure.java.io :as io]
             [compojure.route :as route]
             [compojure.core :refer :all]
             [ring.util.http-response :refer [bad-request]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.nested-params :refer [wrap-nested-params]]
+            [ring.middleware.params :refer [wrap-params]]
             ring.middleware.http-response
             ring.swagger.middleware
             [compojure.api.json :refer :all]))
@@ -43,8 +45,10 @@
   "opinionated chain of middlewares for web apis."
   [handler]
   (-> handler
-    ring.middleware.http-response/catch-response
-    ring.swagger.middleware/catch-validation-errors
-    json-support
-    ex-info-support
-    compojure/api))
+      ring.middleware.http-response/catch-response
+      ring.swagger.middleware/catch-validation-errors
+      json-support
+      ex-info-support
+      wrap-keyword-params
+      wrap-nested-params
+      wrap-params))
