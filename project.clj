93501bfd9154f9eb4223056b540c21932885a606
Juho Teperi <juho.teperi@iki.fi>
2014-05-11 20:26:44 +0300

    Clean project.clj

--- project.clj
+++ project.clj
@@ -13,17 +13,18 @@
                  [prismatic/schema "0.2.2"]
                  [metosin/ring-http-response "0.4.0"]
                  [metosin/ring-swagger "0.8.6"]]
-  :profiles {:thingie {:ring {:handler examples.thingie/app}
-                       :main examples.server}
+  :profiles {:thingie {:ring {:handler examples.thingie/app
+                              :reload-paths ["src" "examples/src"]}
+                       :source-paths ["examples/src"]
+                       :main examples.server
+                       :dependencies [[metosin/ring-swagger-ui "2.0.16-2"]
+                                      [http-kit "2.1.18"]]}
              :dev {:ring {:handler examples.handler/app}
-                   :source-paths ["examples/src"]
                    :plugins [[lein-clojars "0.9.1"]
                              [lein-midje "3.1.3"]
                              [lein-ring "0.8.10"]]
                    :dependencies [[peridot "0.2.2"]
-                                  [http-kit "2.1.18"]
                                   [javax.servlet/servlet-api "2.5"]
-                                  [midje "1.6.3"]
-                                  [metosin/ring-swagger-ui "2.0.16-2"]]}}
-  :aliases {"start-thingie"    ["with-profile" "dev,thingie" "ring" "server"]
-            "http-kit-thingie" ["with-profile" "dev,thingie" "run"]})
+                                  [midje "1.6.3"]]}}
+  :aliases {"start-thingie"    ["with-profile" "thingie" "ring" "server"]
+            "http-kit-thingie" ["with-profile" "thingie" "run"]})
