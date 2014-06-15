fd951ebfbc65aee2f0826d1b9c4fdd372dc5ee07
Tommi Reiman <tommi@iki.fi>
2014-06-15 15:40:38 +0300

    depend on tuned ring-swagger

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [compojure "1.1.8"]
                  [prismatic/schema "0.2.3"]
                  [metosin/ring-http-response "0.4.0"]
-                 [metosin/ring-swagger "0.8.8"]]
+                 [metosin/ring-swagger "0.8.8-models"]]
   :profiles {:thingie {:ring {:handler examples.thingie/app
                               :reload-paths ["src" "examples/src"]}
                        :source-paths ["examples/src"]
