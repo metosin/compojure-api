b7a5b6cc49ac78bc7193bfeca3efcceeca85ddc1
Tommi Reiman <tommi@iki.fi>
2014-06-18 13:59:24 +0300

    update deps

--- project.clj
+++ project.clj
@@ -6,13 +6,13 @@
             :distribution :repo
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [prismatic/plumbing "0.3.1"]
+                 [prismatic/plumbing "0.3.2"]
                  [potemkin "0.3.4"]
                  [cheshire "5.3.1"]
                  [compojure "1.1.8"]
-                 [prismatic/schema "0.2.3"]
+                 [prismatic/schema "0.2.4"]
                  [metosin/ring-http-response "0.4.0"]
-                 [metosin/ring-swagger "0.9.1"]]
+                 [metosin/ring-swagger "0.10.0"]]
   :profiles {:thingie {:ring {:handler examples.thingie/app
                               :reload-paths ["src" "examples/src"]}
                        :source-paths ["examples/src"]
@@ -25,7 +25,7 @@
              :dev {:ring {:handler examples.handler/app}
                    :plugins [[lein-clojars "0.9.1"]
                              [lein-midje "3.1.3"]
-                             [lein-ring "0.8.10"]]
+                             [lein-ring "0.8.11"]]
                    :dependencies [[peridot "0.3.0"]
                                   [javax.servlet/servlet-api "2.5"]
                                   [midje "1.6.3"]]}}
