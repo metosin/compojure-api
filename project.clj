3a5c8d5e670fc7cfb2164354e1159f3c2914b48b
Juho Teperi <juho.teperi@iki.fi>
2014-10-01 21:20:45 +0300

    Travis: run tests using both Clojure 1.6 & 1.7

--- project.clj
+++ project.clj
@@ -27,10 +27,12 @@
                              [lein-ring "0.8.11"]]
                    :dependencies [[peridot "0.3.0"]
                                   [javax.servlet/servlet-api "2.5"]
-                                  [midje "1.6.3"]]}}
+                                  [midje "1.6.3"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}}
   :eastwood {:namespaces [:source-paths]
              :add-linters [:unused-namespaces]}
-  :aliases {"start-thingie"    ["with-profile" "thingie" "ring" "server"]
+  :aliases {"all" ["with-profile" "dev:dev,1.7"]
+            "start-thingie"    ["with-profile" "thingie" "ring" "server"]
             "http-kit-thingie" ["with-profile" "thingie" "run"]
             "aot-uberjar"      ["with-profile" "thingie,uberjar" "do" "clean," "ring" "uberjar"]
             "deploy!"          ^{:doc "Recompile sources, then deploy if tests succeed."}
