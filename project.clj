6a6da910f12ccaaf7469b3725cccf0aa693d3254
Tommi Reiman <tommi@iki.fi>
2015-01-10 15:46:46 +0200

    bump up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/compojure-api "0.16.6"
+(defproject metosin/compojure-api "0.17.0"
   :description "Compojure Api"
   :url "https://github.com/metosin/compojure-api"
   :license {:name "Eclipse Public License"
