6179930ac9656e5a0a549910187f4c7b6c299010
Tommi Reiman <tommi@iki.fi>
2015-05-18 17:48:24 +0300

    prepare for 0.20.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/compojure-api "0.20.3"
+(defproject metosin/compojure-api "0.20.4-SNAPSHOT"
   :description "Compojure Api"
   :url "https://github.com/metosin/compojure-api"
   :license {:name "Eclipse Public License"
