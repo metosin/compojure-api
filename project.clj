3de022624ac52b4c0018f73e3310a8aa4dd68836
Tommi Reiman <tommi@iki.fi>
2015-07-12 11:22:05 +0300

    update deps

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [compojure "1.3.4"]
                  [prismatic/schema "0.4.3"]
                  [org.tobereplaced/lettercase "1.0.0"]
-                 [metosin/ring-http-response "0.6.2"]
+                 [metosin/ring-http-response "0.6.3"]
                  [metosin/ring-swagger "0.21.0-SNAPSHOT"]
                  [metosin/ring-middleware-format "0.6.0"]
                  [backtick "0.3.3"]
@@ -29,7 +29,7 @@
                              [funcool/codeina "0.1.0"]]
                    :dependencies [[peridot "0.4.0"]
                                   [javax.servlet/servlet-api "2.5"]
-                                  [midje "1.7.0-SNAPSHOT"]
+                                  [midje "1.7.0"]
                                   [metosin/scjsv "0.2.0"]
                                   [com.stuartsierra/component "0.2.3"]
                                   [reloaded.repl "0.1.0"]
@@ -38,7 +38,7 @@
                           :reload-paths ["src" "examples/src"]}
                    :source-paths ["examples/src" "examples/dev-src"]
                    :main examples.server}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-RC1"]]}}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :eastwood {:namespaces [:source-paths]
              :add-linters [:unused-namespaces]}
   :codeina {:sources ["src"]
