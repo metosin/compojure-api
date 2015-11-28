11c994801e36f13f597b5a67dfe084d08e6b61cf
Tommi Reiman <tommi@iki.fi>
2015-11-28 14:12:05 +0200

    Cleaned up based on @Deraen's comments.

--- src/compojure/api/logging.clj
+++ src/compojure/api/logging.clj
@@ -1,20 +1,22 @@
-(ns compojure.api.logging
+(ns ^:no-doc compojure.api.logging
+  "Internal Compojure-api logging utility"
   (:require [clojure.string :as str]))
 
-;; default to console logging
-(defn log! [level x & more]
-  (let [log (fn [level more] (println (.toUpperCase (name level)) (str/join " - " more)))]
-    (if (instance? Throwable x)
-      (do
-        (log level more)
-        (.printStackTrace x))
-      (log level (into [x] more)))))
+;; Cursive-users
+(declare log!)
 
-;; use c.t.l logging if available
+;; use c.t.l logging if available, default to console logging
 (if (find-ns 'clojure.tools.logging)
   (eval
     `(do
        (require 'clojure.tools.logging)
        (defmacro ~'log! [& ~'args]
          `(do
-            (clojure.tools.logging/log ~@~'args))))))
+            (clojure.tools.logging/log ~@~'args)))))
+  (let [log (fn [level more] (println (.toUpperCase (name level)) (str/join " " more)))]
+    (defn log! [level x & more]
+      (if (instance? Throwable x)
+        (do
+          (log level more)
+          (.printStackTrace x))
+        (log level (into [x] more))))))
