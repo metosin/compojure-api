ed097657878b95e79eee98f46bcfe0a74977a5d3
Tommi Reiman <tommi@iki.fi>
2016-01-10 22:43:54 +0200

    Convert swagger_ordering_test

--- test/compojure/api/swagger_ordering_test.clj
+++ test/compojure/api/swagger_ordering_test.clj
@@ -1,15 +1,17 @@
 (ns compojure.api.swagger-ordering-test
   (:require [compojure.api.sweet :refer :all]
-            [midje.sweet :refer :all]))
+            [midje.sweet :refer :all]
+            [compojure.api.routing :as r]
+            [compojure.api.routes :as routes]))
 
-(defroutes* more-routes
-  (GET* "/6" [] identity)
-  (GET* "/7" [] identity)
-  (GET* "/8" [] identity))
+(def more-routes
+  (routes*
+    (GET* "/6" [] identity)
+    (GET* "/7" [] identity)
+    (GET* "/8" [] identity)))
 
 (facts "with 10+ routes"
   (let [app (api
-              (swagger-docs)
               (context* "/a" []
                 (GET* "/1" [] identity)
                 (GET* "/2" [] identity)
@@ -23,16 +25,17 @@
                   (GET* "/10" [] identity))))]
 
     (fact "swagger-api order is maintained"
-      (->> app
-           meta
-           :routes
-           (map first)) => ["/a/1"
-                            "/a/2"
-                            "/a/3"
-                            "/a/b/4"
-                            "/a/b/5"
-                            "/a/c/6"
-                            "/a/c/7"
-                            "/a/c/8"
-                            "/a/c/9"
-                            "/a/c/10"])))
+      (-> app
+          r/get-routes
+          routes/->ring-swagger
+          :paths
+          keys) => ["/a/1"
+                    "/a/2"
+                    "/a/3"
+                    "/a/b/4"
+                    "/a/b/5"
+                    "/a/c/6"
+                    "/a/c/7"
+                    "/a/c/8"
+                    "/a/c/9"
+                    "/a/c/10"])))
