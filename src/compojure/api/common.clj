b73773ce1b1a2407cf4dd40a33f0d23615821b19
Tommi Reiman <tommi@iki.fi>
2016-01-12 00:20:09 +0200

    Remove dead code

--- src/compojure/api/common.clj
+++ src/compojure/api/common.clj
@@ -1,60 +1,10 @@
-(ns compojure.api.common
-  (:require [org.tobereplaced.lettercase :as lc]))
-
-(defn path-vals
-  "Returns vector of tuples containing path vector to the value and the value."
-  [m]
-  (letfn
-    [(pvals [l p m]
-       (reduce
-         (fn [l [k v]]
-           (if (map? v)
-             (pvals l (conj p k) v)
-             (cons [(conj p k) v] l)))
-         l m))]
-    (pvals [] [] m)))
-
-(defn assoc-in-path-vals
-  "Re-created a map from it's path-vals extracted with (path-vals)."
-  [c] (reduce (partial apply assoc-in) {} c))
-
-(defmacro re-resolve
-  "Extracts original var from a (potemkined) var or a symbol or returns nil"
-  [x]
-  (let [evaluated (if (symbol? x) x (eval x))
-        resolved (cond
-                   (var? evaluated) evaluated
-                   (symbol? evaluated) (resolve evaluated)
-                   :else nil)
-        metadata (meta resolved)]
-    (if metadata
-      (let [s (symbol (str (:ns metadata) "/" (:name metadata)))]
-        `(var ~s)))))
-
-(defn eval-re-resolve [x] (eval `(re-resolve ~x)))
-
-(defn assoc-map-ordered
-  "assocs a value into a map forcing the implementation to be
-   clojure.lang.PersistentArrayMap instead of clojure.lang.PersistentHashMap,
-   thus retaining the insertion order. O(n)."
-  [m k v] (apply array-map (into (vec (apply concat m)) [k v])))
+(ns compojure.api.common)
 
 (defmacro map-of
   "creates map with symbol names as keywords as keys and
    symbol values as values."
   [& syms]
   `(zipmap ~(vec (map keyword syms)) ~(vec syms)))
-
-(defn ->CamelCase [x]
-  (lc/capitalized x))
-
-(defmacro local-bindings []
-  (->> (keys &env)
-       (map (fn [k] [`'~k k]))
-       (into {})))
-
-(defmacro get-local [s]
-  `(get (local-bindings) ~s))
 
 (defn plain-map?
   "checks whether input is a map, but not a record"
--- test/compojure/api/common_test.clj
+++ test/compojure/api/common_test.clj
@@ -1,64 +1,7 @@
 (ns compojure.api.common-test
   (:require [compojure.api.common :refer :all]
-            [midje.sweet :refer :all]
-            potemkin))
-
-(fact "path-vals"
-  (let [original {:a {:b {:c 1
-                          :d 2}
-                      :e 3}}
-        target [[[:a :b :d] 2]
-                [[:a :b :c] 1]
-                [[:a :e] 3]]]
-    (path-vals original) => (contains target :in-any-order)
-    (assoc-in-path-vals target) => original))
-
-(potemkin/import-vars [clojure.walk walk])
-
-(fact "re-resolve"
-
-  (fact "potemkin'd var is imported locally"
-    #'walk => #'compojure.api.common-test/walk)
-
-  (fact "non-symbol/var resolves to nil"
-    (re-resolve 1) => nil)
-
-  (fact "re-resolve to the rescue!"
-    (re-resolve walk) => #'clojure.walk/walk
-    (re-resolve 'walk) => #'clojure.walk/walk
-    (re-resolve #'walk) => #'clojure.walk/walk))
-
-(defmacro re-resolve-in-compile-time [sym]
-  (let [resolved (re-resolve sym)]
-    `~resolved))
-
-(defmacro eval-re-resolve-in-compile-time [sym]
-  (let [resolved (eval-re-resolve sym)]
-    `~resolved))
-
-(fact "re-resolve in compile-time"
-  (fact "re-resolve does not work with macros"
-    (re-resolve-in-compile-time 'walk) => nil)
-  (fact "eval-re-resolve works with macros"
-    (eval-re-resolve-in-compile-time 'walk) => #'clojure.walk/walk))
-
-(fact "assoc-map"
-  (fact "assoc for array-map loses its order"
-    (keys (reduce (partial apply assoc) (array-map) (map-indexed vector (range 100)))) =not=> (range 100))
-  (fact "assoc-map-ordered for array-map retains its order"
-    (keys (reduce (partial apply assoc-map-ordered) (array-map) (map-indexed vector (range 100)))) => (range 100)))
+            [midje.sweet :refer :all]))
 
 (fact "map-of"
   (let [a 1 b true c [:abba :jabba]]
     (map-of a b c) => {:a 1 :b true :c [:abba :jabba]}))
-
-(fact "->CamelCase"
-  (->CamelCase "olipa-kerran") => "OlipaKerran")
-
-(fact "get-local allows local symbol resolution"
-  (let [a1 (get-local 'a)
-        a "kikka"
-        a2 (get-local 'a)]
-    a1 => nil
-    a => "kikka"
-    a2 => "kikka"))
