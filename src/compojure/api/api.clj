a404dfa4a6c6dfbd67037a2f8288372143595950
Juho Teperi <juho.teperi@iki.fi>
2016-01-18 23:08:10 +0200

    Embed api-middleware docstring in api and defapi

--- src/compojure/api/api.clj
+++ src/compojure/api/api.clj
@@ -7,7 +7,8 @@
             [clojure.tools.macro :as macro]
             [ring.swagger.swagger2 :as swagger2]))
 
-(defn api
+(defn
+  ^{:doc (str
   "Returns a ring handler wrapped in compojure.api.middleware/api-middlware.
   Creates the route-table at run-time and passes that into the request via
   ring-swagger middlewares. The mounted api-middleware can be configured by
@@ -18,7 +19,10 @@
         (context \"/api\" []
           ...))
 
-  ... see compojure.api.middleware/api-middleware for possible options."
+  API middleware options:
+
+  " (:doc (meta #'compojure.api.middleware/api-middleware)))}
+  api
   [& body]
   (let [[options handlers] (common/extract-parameters body)
         handler (apply core/routes handlers)
@@ -31,9 +35,14 @@
                                                   :lookup lookup}))]
     (routes/create nil nil {} [handler] api-handler)))
 
-(defmacro defapi
+(defmacro
+  ^{:doc (str
   "Defines an api. The name may optionally be followed by a doc-string
-  and metadata map."
+  and metadata map.
+
+  API middleware options:
+  " (:doc (meta #'compojure.api.middleware/api-middleware)))}
+  defapi
   [name & body]
   (let [[name body] (macro/name-with-attributes name body)]
     `(def ~name (api ~@body))))
