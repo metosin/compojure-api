c0683a34917f9b66b62cee6b5bea6c19adeb8cf7
Tommi Reiman <tommi@iki.fi>
2016-02-01 08:43:11 +0200

    Test AOT with sample app

--- project.clj
+++ project.clj
@@ -22,6 +22,7 @@
                        :source-paths ["examples/src"]
                        :dependencies [[org.clojure/clojure "1.7.0"]
                                       [http-kit "2.1.19"]
+                                      [reloaded.repl "0.2.1"]
                                       [com.stuartsierra/component "0.3.1"]]}
              :dev {:repl-options {:init-ns user}
                    :plugins [[lein-clojars "0.9.1"]
