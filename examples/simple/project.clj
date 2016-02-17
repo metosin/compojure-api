ecee8863367ee25cb510fca2c8a91e91e6d559fb
Juho Teperi <juho.teperi@iki.fi>
2016-02-17 16:32:59 +0200

    Update simple example project.clj

--- examples/simple/project.clj
+++ examples/simple/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.11.0"] ; required due to bug in `lein-ring uberwar`
-                 [metosin/compojure-api "1.0.0-RC1"]]
+                 [metosin/compojure-api "1.0.0"]]
   :ring {:handler simple.handler/app}
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
