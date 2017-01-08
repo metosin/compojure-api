2b6fd29cd65f8127f04779765838ff5c6096f4bf
Tommi Reiman <tommi@iki.fi>
2017-01-08 22:40:19 +0200

    Don't auto-coerce YAML responses

--- src/compojure/api/middleware.clj
+++ src/compojure/api/middleware.clj
@@ -85,8 +85,7 @@
 
 (def default-coercion-options
   {:body {:default (constantly nil)
-          :formats {"application/json" coerce/json-schema-coercion-matcher
-                    "application/yaml" coerce/query-schema-coercion-matcher}}
+          :formats {"application/json" coerce/json-schema-coercion-matcher}}
    :string coerce/query-schema-coercion-matcher
    :response {:default (constantly nil)
               ;; TODO: don't auto-coerce the JSON responses? => would be a breaking change in 1.2
