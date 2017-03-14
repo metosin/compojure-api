9316f0b37d8b62a42a326b8a7f603f951af2e522
Miikka Koskinen <miikka@metosin.fi>
2017-03-14 18:43:45 +0200

    Make the async example work again
    The muuntaja SNAPSHOT was broken.

--- examples/async/project.clj
+++ examples/async/project.clj
@@ -1,9 +1,8 @@
 (defproject example "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [metosin/compojure-api "1.2.0-SNAPSHOT" :exclude [compojue, metosin/muuntaja]]
-                 [metosin/muuntaja "0.2.0-SNAPSHOT"]
-                 [ring/ring "1.6.0-beta7"]
+                 [metosin/compojure-api "1.2.0-SNAPSHOT" :exclude [compojure, metosin/muuntaja]]
+                 [ring/ring "1.6.0-RC1"]
                  [compojure "1.6.0-beta3"]
                  [manifold "0.1.6-alpha6"]
                  [org.clojure/core.async "0.3.441"]]
