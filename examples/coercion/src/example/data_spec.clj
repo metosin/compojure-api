3431848a353e178ae2a88bc48ec11fe2315f8212
Tommi Reiman <tommi@iki.fi>
2017-06-30 16:54:00 +0300

    data-specs wrap spec records already. awesome!

--- examples/coercion/src/example/data_spec.clj
+++ examples/coercion/src/example/data_spec.clj
@@ -1,7 +1,6 @@
 (ns example.data-spec
   (:require [compojure.api.sweet :refer [context POST resource]]
-            [ring.util.http-response :refer [ok]]
-            [spec-tools.spec :as spec]))
+            [ring.util.http-response :refer [ok]]))
 
 (def routes
   (context "/data-spec" []
@@ -10,15 +9,15 @@
 
     (POST "/plus" []
       :summary "plus with clojure.spec using data-specs"
-      :body-params [x :- spec/int?, {y :- spec/int? 0}]
-      :return {:total spec/int?}
+      :body-params [x :- int?, {y :- int? 0}]
+      :return {:total int?}
       (ok {:total (+ x y)}))
 
     (context "/plus" []
       (resource
         {:get
          {:summary "data-driven plus with clojure.spec using data-specs"
-          :parameters {:query-params {:x spec/int?, :y spec/int?}}
-          :responses {200 {:schema {:total spec/int?}}}
+          :parameters {:query-params {:x int?, :y int?}}
+          :responses {200 {:schema {:total int?}}}
           :handler (fn [{{:keys [x y]} :query-params}]
                      (ok {:total (+ x y)}))}}))))
