7d5591f507d989cecb543bd55aa0de0a0ee37384
Miikka Koskinen <miikka@metosin.fi>
2019-12-20 16:21:55 +0200

    Release 2.0.0-alpha31

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/compojure-api "2.0.0-alpha30"
+(defproject metosin/compojure-api "2.0.0-alpha31"
   :description "Compojure Api"
   :url "https://github.com/metosin/compojure-api"
   :license {:name "Eclipse Public License"
