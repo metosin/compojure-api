7eeae738c3d8003522c9b2f64478028f5dfda7ab
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-04-21 23:01:52 -0500

    bump snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/compojure-api "2.0.0-alpha31"
+(defproject metosin/compojure-api "2.0.0-alpha32-SNAPSHOT"
   :description "Compojure Api"
   :url "https://github.com/metosin/compojure-api"
   :license {:name "Eclipse Public License"
