ce5debba4bbc368d36324bead99906d35cb9c041
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-04-22 23:39:36 -0500

    Release :alpha

--- project.clj
+++ project.clj
@@ -72,10 +72,10 @@
                                      :username [:gpg :env/clojars_user]
                                      :password [:gpg :env/clojars_token]
                                      :sign-releases false}]
-                        ["releases" {:url "https://clojars.org/repo"
-                                     :username [:gpg :env/clojars_user]
-                                     :password [:gpg :env/clojars_token]
-                                     :sign-releases false}]]
+                        ["release" {:url "https://clojars.org/repo"
+                                    :username [:gpg :env/clojars_user]
+                                    :password [:gpg :env/clojars_token]
+                                    :sign-releases false}]]
   :release-tasks [["clean"]
                   ["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
