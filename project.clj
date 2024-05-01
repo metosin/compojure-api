7d790eb096bba5f3c1b7c4580ce0a54e5acc54a1
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2024-05-01 01:59:06 +0000

    Version 1.1.14

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/compojure-api "1.1.14-SNAPSHOT"
+(defproject metosin/compojure-api "1.1.14"
   :description "Compojure Api"
   :url "https://github.com/metosin/compojure-api"
   :license {:name "Eclipse Public License"
