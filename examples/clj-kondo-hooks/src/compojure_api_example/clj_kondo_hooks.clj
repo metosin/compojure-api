852c54dc2924da1cb5e54e2e365b4c130d1244c4
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-08-27 20:04:26 -0500

    [skip ci]

--- examples/clj-kondo-hooks/src/compojure_api_example/clj_kondo_hooks.clj
+++ examples/clj-kondo-hooks/src/compojure_api_example/clj_kondo_hooks.clj
@@ -1,2 +1,7 @@
 (ns compojure-api-example.clj-kondo-hooks
-  (:require []))
+  (:require [compojure.api.sweet :as sweet]
+            [ring.util.http-response :as resp]))
+
+(sweet/GET "/30" [] (resp/ok {:result 30}))
+(sweet/GET "/30" req
+           (resp/ok {:result (:body req)}))
