a9d977989075dd70d5dc199fef90878b9c16babd
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2024-08-30 17:10:34 -0500

    wip

--- examples/clj-kondo-hooks/src/compojure_api_example/clj_kondo_hooks.clj
+++ examples/clj-kondo-hooks/src/compojure_api_example/clj_kondo_hooks.clj
@@ -23,11 +23,11 @@
 ;; intentionally blank
 
 (core/GET "/30" [] (resp/ok {:result 30}))
-(core/GET "/30" [] (:ok)) ;; src/compojure_api_example/clj_kondo_hooks.clj:7:20: error: keyword :ok is called with 0 args but expects 1 or 2
+(core/GET "/30" [] (:ok)) ;; src/compojure_api_example/clj_kondo_hooks.clj:26:20: error: keyword :ok is called with 0 args but expects 1 or 2
 (core/GET "/30" [] '(:ok))
 
 (sweet/GET "/30" [] (resp/ok {:result 30}))
-(sweet/GET "/30" [] (:ok)) ;; src/compojure_api_example/clj_kondo_hooks.clj:7:20: error: keyword :ok is called with 0 args but expects 1 or 2
+(sweet/GET "/30" [] (:ok)) ;; src/compojure_api_example/clj_kondo_hooks.clj:30:21: error: keyword :ok is called with 0 args but expects 1 or 2
 (sweet/GET "/30" [] '(:ok))
 
 (core/GET "/30" req (resp/ok {:result (:body req)}))
