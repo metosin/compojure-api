34670f216393e0d3ecd53ee9fa3c5a95763fcb9a
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2024-08-30 14:25:11 -0500

    wip

--- scripts/regen-kondo.clj
+++ scripts/regen-kondo.clj
@@ -10,3 +10,4 @@ bb -f ./scripts/regen_kondo_config.clj
 # rename to .clj
 cp src/compojure/api/common.cljc resources/clj-kondo.exports/metosin/compojure-api/compojure/api/common.clj
 cp src/compojure/api/core.cljc resources/clj-kondo.exports/metosin/compojure-api/compojure/api/core.clj
+cp src/compojure/api/meta.cljc resources/clj-kondo.exports/metosin/compojure-api/compojure/api/meta.clj
