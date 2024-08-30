6bb6bb869cf07e42df473a9f132df30f38013244
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2024-08-30 17:09:54 -0500

    wip

--- examples/clj-kondo-hooks/src/compojure_api_example/clj_kondo_hooks.clj
+++ examples/clj-kondo-hooks/src/compojure_api_example/clj_kondo_hooks.clj
@@ -1,11 +1,34 @@
 (ns compojure-api-example.clj-kondo-hooks
-  (:require ;[compojure.api.sweet :as sweet]
+  (:require [compojure.api.sweet :as sweet]
             [compojure.api.core :as core]
-            [ring.util.http-response :as resp]))
+            [ring.util.http-response :as resp]
+            ;; intentionally blank
+            ;; intentionally blank           
+            ;; intentionally blank
+            ;; intentionally blank
+            ;; intentionally blank
+            ;; intentionally blank
+            ;; intentionally blank
+            ;; intentionally blank
+            ))
+;; intentionally blank
+;; intentionally blank           
+;; intentionally blank
+;; intentionally blank
+;; intentionally blank
+;; intentionally blank
+;; intentionally blank           
+;; intentionally blank
+;; intentionally blank
+;; intentionally blank
 
 (core/GET "/30" [] (resp/ok {:result 30}))
 (core/GET "/30" [] (:ok)) ;; src/compojure_api_example/clj_kondo_hooks.clj:7:20: error: keyword :ok is called with 0 args but expects 1 or 2
 (core/GET "/30" [] '(:ok))
+
+(sweet/GET "/30" [] (resp/ok {:result 30}))
+(sweet/GET "/30" [] (:ok)) ;; src/compojure_api_example/clj_kondo_hooks.clj:7:20: error: keyword :ok is called with 0 args but expects 1 or 2
+(sweet/GET "/30" [] '(:ok))
 
 (core/GET "/30" req (resp/ok {:result (:body req)}))
 (core/GET "/30" [:as req] (resp/ok {:result (:body req)}))
